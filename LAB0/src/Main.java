fd8b5f4b3095a4795ff37ee1067ffb2df2df63c5
a.surkova <a.surkova@g.nsu.ru>
2024-03-11 18:20:33 +0700

    delete lab 0

--- LAB0/src/Main.java
+++
@@ -1,8 +0,0 @@
-import CSVParser.CSVParser;
-
-public class Main {
-    public static void main(String[] args) {
-        CSVParser csvParser = new CSVParser();
-        csvParser.parseFile("input.txt", "output.txt");
-    }
-}

b9f35cd598c8e2b93c2660f2b9adcc405d166be6
a.surkova <a.surkova@g.nsu.ru>
2024-04-15 00:33:32 +0700

    add LAB1

--- LAB1/src/main/java/org/example/CalcIterator.java
+++ LAB1/src/main/java/org/example/CalcIterator.java
@@ -8,7 +8,6 @@ import org.example.Exception.FabricException;
 import org.example.Factory.CommandFactory;
 import org.example.Parser.Parser;
 
-import java.io.IOException;
 import java.lang.reflect.AnnotatedArrayType;
 import java.util.ArrayList;
 
@@ -22,7 +21,7 @@ public class CalcIterator {
         this.parser = parser;
     }
 
-    public void calculation() throws IOException {
+    public void calculation(){
         var commandFactory = new CommandFactory(CONFIG_PATH);
         log.logInfo("Start calculation...");
         Argument argument;
--- LAB1/src/main/java/org/example/Exception/FabricException.java
+++
@@ -1,7 +0,0 @@
-package org.example.Exception;
-
-public class FabricException extends Exception{
-    public FabricException(String message) {
-        super(message);
-    }
-}

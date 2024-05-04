2b005be838f34654575f2a574e64234bf2f7e5e4
a.surkova <a.surkova@g.nsu.ru>
2024-05-04 16:59:33 +0700

    add files

---
+++ LAB2/core/src/com/mygdx/game/Service/Animation/AnimationItem.java
@@ -0,0 +1,14 @@
+package com.mygdx.game.Service.Animation;
+
+public class AnimationItem {
+    private String name;
+    private int countFrames;
+
+    public int getCountFrames() {
+        return countFrames;
+    }
+
+    public String getName() {
+        return name;
+    }
+}

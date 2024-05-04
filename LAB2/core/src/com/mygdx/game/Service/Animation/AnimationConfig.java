304573add6e3a4b00c8606da013c73188c01e49f
a.surkova <a.surkova@g.nsu.ru>
2024-05-04 16:59:33 +0700

    add files

---
+++ LAB2/core/src/com/mygdx/game/Service/Animation/AnimationConfig.java
@@ -0,0 +1,16 @@
+package com.mygdx.game.Service.Animation;
+
+import java.util.ArrayList;
+
+public class AnimationConfig {
+    private String animName;
+    private ArrayList<AnimationItem> animations;
+
+    public ArrayList<AnimationItem> getAnimations() {
+        return animations;
+    }
+
+    public String getAnimName() {
+        return animName;
+    }
+}
